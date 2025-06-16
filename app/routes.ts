eb1f4e00997022abacc10152ad0689c98bf6ffce
AhmadAli88 <xevenahmad@gmail.com>
2025-06-16 01:55:49 -0700

    nested routes, layout, ssr, outlet

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,13 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   route("about", "routes/about.tsx"),
     route("post/:postId", "routes/post.tsx"),
+    layout("routes/dashboard.tsx",[
+      ...prefix("ahmad", [
+    route("finances", "routes/finances.tsx"),
+    route("personalInfo", "routes/personalInfo.tsx"),
+      ])
+    ]),
 ] satisfies RouteConfig;
--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Form, redirect, useLoaderData } from "react-router-dom";
 
-const Post = () => {
-  return (
-    <div>Post</div>
-  )
+// Define the shape of the loader data
+type LoaderData = {
+  postId: string;
+};
+export async function clientAction({
+  params,
+}: {
+  params: { postId: string };
+}): Promise<Response> {
+  const { postId } = params;
+  // Simulate a delete action
+   await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${postId}`,
+    {
+      method: 'DELETE',
+    })
+  return redirect(`/`);
 }
 
-export default Post
+// Loader function with correct return type
+export async function loader({
+  params,
+}: {
+  params: { postId: string };
+}): Promise<LoaderData> {
+  const { postId } = params;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${postId}`
+  );
+  const data = await response.json();
+  return data;
+}
+
+
+
+// React component that uses the loader data
+const Post: React.FC = () => {
+  const data = useLoaderData() as any;
+
+  return <div>
+    <p>Post: {data.title}</p>
+    <p>Body: {data.body}</p>
+    <Form method="delete">
+      <button type="submit">Delete</button>
+    </Form>
+    </div>;
+};
+
+export default Post;
